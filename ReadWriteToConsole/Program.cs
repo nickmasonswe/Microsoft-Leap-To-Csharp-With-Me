a230120c55dcb92d0a1f8046bf922cd80ebbac93
nickmasonswe <nickmasonswe@gmail.com>
2024-09-04 08:40:33 -0400

    Read and Write to the console + add notes on initializing new projects

---
+++ ReadWriteToConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// comments like js/ts
+/* Multi line comment as such */
+//Writing and Reading input via console
+using System;
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What\'s your name?");
+      string input = Console.ReadLine();
+      Console.WriteLine($" Hi, {input}!");
+    }
+  }
+}
