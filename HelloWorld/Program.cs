24ecdc90bf3416d53aa773028ef0e225edc33c64
nickmasonswe <nickmasonswe@gmail.com>
2024-09-04 08:43:02 -0400

    _

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿
+//Use dotnet run to run programs.
+//Use path to create new projects via c# dev kit extension. > .NET: new ....
+using System;
 
 class Program
 {
- static void Main(string[] args)
- {
-    // Code here
+  static void Main(string[] args)
+  {
     Console.WriteLine("Hello, World!");
   }
 }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef3b2d0f80454380e1e57a1cda0c6c773b23a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
